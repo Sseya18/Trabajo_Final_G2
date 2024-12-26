5cc88dda74fdd1b2907d5cf67f352b6a4f5f1fa6
Josemi Vásquez <josemi.vahu@gmail.com>
2024-12-26 15:08:13 -0500

    Clientes Vistas y Modelo

--- gestionclientesmicroservicios/src/main/java/com/cobranza/gestionclientesmicroservicios/entidades/Cliente.java
+++ gestionclientesmicroservicios/src/main/java/com/cobranza/gestionclientesmicroservicios/entidades/Cliente.java
@@ -9,23 +9,18 @@ import java.time.LocalDate;
 @Entity
 @Data
 public class Cliente {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String nombre;
-
-    private String apellido;
+    private Long ruc;
 
     @Column(unique = true, nullable = false)
-    private String email;
-
+    private String nombre;
+    @Column(unique = true)
     private String telefono;
-
+    @Column(unique = true, nullable = false)
     private String direccion;
-
     private LocalDate fechaRegistro;
 
-    private String estado; // "ACTIVO" o "INACTIVO"
+    private Integer nroDeudoresAsignados;
+    private Float montoTotalDeuda;
+    private Float montoSaldado;
 }
